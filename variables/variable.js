32694f3dca19442b4ad51db56436b3a67851326b
jatink2000 <jatink8059@gmail.com>
2026-02-17 11:55:56 +0530

    sk

--- variables/variable.js
+++ variables/variable.js
@@ -27,3 +27,68 @@
 
         // 5. variable name cannot contain any extra space . 
                     // gro_ot=39
+
+
+        // how to modify the variable value : 
+
+        // groot=20 
+        // document.writeln(groot) // 20 
+        // groot=25
+        // document.writeln(groot) // 25
+
+
+        // how to declare the variable : 
+
+        // 1. var : modify & redeclare 
+                // var groot=30
+                // document.writeln(groot)
+                // groot=40 // change /modify 
+                // document.writeln(groot)
+
+
+
+                // var groot=90
+                // document.writeln(groot)
+
+
+
+
+        // 2. let :  modify but redeclare not allow inside same scope . 
+                // let groot=20 
+                // document.writeln(groot) // 20
+                // groot=60 
+                // document.writeln(groot) // 60
+
+
+                // {       let groot=90 
+                //         document.writeln(groot) // 90  
+                // }
+
+
+        // 3. const :  constant variable . 
+                // const groot=50 
+                // document.writeln(groot)
+                // groot=80
+                // document.writeln(groot)
+
+
+
+                // third variable : 
+                // let a=50
+                // let b=10 
+                // let c=a  // 50 
+                // a=b // 10 
+                // b=c // 50 
+                // document.writeln(a)
+                // document.writeln(b)
+
+
+                // operators  : 
+                let a=50
+                let b=10 
+                a=a+b // 50+10 : 60 
+                b=a-b // 60-10 : 50 
+                a=a-b //  60-50 : 10 
+                document.writeln(a)
+                document.writeln(b)
+
