806de8c99e9d08bd640bef15333cbd3077938671
jatink2000 <jatink8059@gmail.com>
2026-02-19 11:55:19 +0530

    dj

---
+++ control statment/controlstatment.js
@@ -0,0 +1,79 @@
+
+
+// controlstatment : 
+
+// 1. if : 
+    // syntex : 
+
+    // if(condition){
+    //     code to be excute . 
+    // }
+
+
+    // let a=prompt("enter a number")
+    // if(a<100){
+    //     document.writeln("your number is less then 100.")
+    // }
+
+
+// 2. else : 
+
+    // syntex : 
+
+    // if(condition){
+    //     code to be excute . 
+    // }
+    // else {
+    //     code to be excute . statment 2
+    // }
+
+
+    // let a=prompt("enter a number")
+    // if(a<100){
+    //     document.writeln("your number is less then 100.")
+    // }
+    // else{
+    //     document.writeln("your number is greater then 100.")
+    // }
+
+
+// 3. else if : 
+
+   // syntex : 
+
+    // if(condition){
+    //     code to be excute . statment 1
+    // }
+    // else if(condition){
+    //     code to be excute . statment 2
+    // }
+    // else {
+    //     code to be excute . statment 3
+    // }
+
+
+    // let a=prompt("enter a number")
+    // if(a<100){
+    //     document.writeln("your number is less then 100.")
+    // }
+    // else if(a<200){
+    //     document.writeln("your number is less then 200 but greater then 100.")
+    // }
+    // else if(a<300){
+    //     document.writeln("your number is less then 300 but greater then 200.")
+    // }
+    // else{
+    //     document.writeln("your number is greater then 300.")
+    // }
+
+
+
+    // Q 1 :
+    let a=prompt("enter first number")
+    let b=prompt("enter second number")
+    if(a>b){
+        document.writeln("first number is maximum ",a)
+    }
+    else{
+        document.writeln("second number is maximum ",b)
+    }
---
+++ operators/operatoes.js
@@ -0,0 +1,55 @@
+
+
+
+
+// 1. arithmatic operators : 
+    // +,-,*,/,% (modules)
+
+    // let a=30 
+    // let b=7
+    // document.writeln("sum is ",a%b) // variable 
+    // document.writeln(34+12) // value 
+
+
+// 2. assigment operators : =,+=,-=,*=,/=,%= 
+
+    // let a=20
+    // a%=5
+    // document.writeln(a)
+
+
+// 3. comparision operators  : ==,<,>,<=,>=,!= (not)
+    
+    // let a=3
+    // let b=3
+    // document.writeln(a==b) // 
+
+
+// 4. logical : 
+    // 1. && (and) : all
+    // let a=3
+    // let b=18
+    // document.writeln(a<b && a>b) 
+
+
+    // 2. || (or) : anyone
+    
+    // let a=3
+    // let b=18
+    // document.writeln(a>b || a>b) 
+
+
+    // 3. ! (not) : 
+    // !true : false 
+    // !false : true 
+
+    // let a=3
+    // let b=18
+    // document.writeln(!(a>b)) 
+
+
+    // user input : 
+    let a=parseInt(prompt("enter a number")) 
+    let b=parseInt(prompt("enter a number"))
+    let c=a+b
+    document.writeln("your number sum is ",c)
