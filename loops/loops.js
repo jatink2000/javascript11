5594db33c33ae2be142a54171c6b23a2be684917
jatink2000 <jatink8059@gmail.com>
2026-03-06 11:58:44 +0530

    h

--- loops/loops.js
+++ loops/loops.js
@@ -316,26 +316,26 @@
 
 
 
-i=1
-while(i<=5){
-    k=5
-    while(k>i){
-        document.writeln("&nbsp;&nbsp;")
-        k--
-    }
-    j=1
-    while(j<=5){
-        if(i==1||i==5||j==1||j==5){
-            document.writeln("*")
-        }
-        else{
-            document.writeln("&nbsp;&nbsp;")
-        }
-        j++
-    }
-    document.writeln("<br/>")
-    i++
-}
+// i=1
+// while(i<=5){
+//     k=5
+//     while(k>i){
+//         document.writeln("&nbsp;&nbsp;")
+//         k--
+//     }
+//     j=1
+//     while(j<=5){
+//         if(i==1||i==5||j==1||j==5){
+//             document.writeln("*")
+//         }
+//         else{
+//             document.writeln("&nbsp;&nbsp;")
+//         }
+//         j++
+//     }
+//     document.writeln("<br/>")
+//     i++
+// }
 
 
 
@@ -357,4 +357,35 @@ while(i<=5){
 //     *
 
 
+
+// Q 1 
+
+
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         if(i==1||j==1 || (i==3 && j==3) || (i==3 && j==4) || (i==3 && j==5) || (i==4 && j==3) || (i==4 && j==5)|| (i==5 && j==3) || (i==5 && j==2) || (i==5 && j==5)){
+//             document.writeln("*")
+//         }
+//         else{
+//             document.writeln("&nbsp;&nbsp;")
+//         }
+//     }
+//     document.writeln("<br/>")
+// }
+
+
+
+// Q 2
+
+for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+        if((i==1&&j==1)|| (i==1&&j==2) || (i==1&&j==3)|| (i==2&&j==1)|| (i==2&&j==3)|| (i==3&&j==1)|| (i==3&&j==2) || (i==3&&j==3)||(i==3&&j==5)|| (i==4&&j==3)||(i==4&&j==4)|| (i==5&&j==3)){
+            document.writeln("*")
+        }
+        else{
+            document.writeln("&nbsp;&nbsp;")
+        }
+    }
+    document.writeln("<br/>")
+}
 
