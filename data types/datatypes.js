a2ccbe560b6dcb87f7e593f4e118e6d4724ed579
jatink2000 <jatink8059@gmail.com>
2026-03-07 11:19:00 +0530

    hs

---
+++ data types/datatypes.js
@@ -0,0 +1,55 @@
+
+
+// data types : a varaiable can store different types of value/data in java script , and these data's types is known as data type . 
+
+// types of data type : there are 2 types of data type . 
+
+// 1. primitive data types : the primitive data types can hold a single value/data . 
+    // 1. number : it can represent the numerical values . 
+        // let a=45 
+        // let b=4.6 
+
+
+    // 2. string : it can represent the single and double alphbate . 
+        // let a='H'
+        // let b="Hello"
+
+
+    // 3. boolean : it can represent the true and false value . 
+        // let a=true 
+        // let b=false
+        
+    
+    // 4. undefined : it can represent the not defined value . 
+        // let a;
+        // console.log(a)
+
+
+    // 5. null : it can represent the empty or null variable . 
+        // let a=null 
+
+
+
+// 2. non primitive data types : the non primitive data types can hold the multiple values / data . 
+
+    // 1. array : 
+    // let a=["c","c++","python","js"]
+
+
+    // 2. object : 
+    // let students={
+    //     "name":"ravi kumar",
+    //     "city":"jaipur",
+    //     "course":"web designing"
+    // }
+
+
+    // 3. function : 
+    // function tech(){
+    //     document.writeln("hello")
+    // }
+
+    // tech()
+
+
+    
