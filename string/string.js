d71d90ce12f47cc54f33122a239398fcbfdc00be
jatink2000 <jatink8059@gmail.com>
2026-03-09 12:11:21 +0530

    sh

---
+++ string/string.js
@@ -0,0 +1,158 @@
+
+
+// string : string can store the multiple text . it can store the single and double quotes . 
+
+// let a="hello groot"
+// let b='helo groot'
+
+
+
+// quotes inside quotes : 
+
+// 1. double quotes : 
+// let a='hello "groot" '
+// document.writeln(a) 
+
+
+// 2. single quotes : 
+// let a="it's groot"
+// document.writeln(a)
+
+
+
+
+// special char : 
+// let a="hello \"groot\" " 
+// document.writeln(a)
+
+
+// let a='it\'s groot '
+// document.writeln(a)
+
+
+// let a="hello groot academy\\ groot software"
+// document.writeln(a)
+
+
+
+
+// let a=`hello "groot" & it's groot ` 
+// document.writeln(a)
+
+
+
+// let a=`hello
+//         groot 
+//         academy `
+// document.writeln(a)
+
+
+
+// variable inside a string : 
+// interplation :`${variable}` 
+
+// let name="groot"
+// let b=2000
+// let a=`my name is ${name} . ${b} `
+// document.writeln(a)
+
+
+
+// string methods : 
+// 1. string length : 
+// let a="hello groot"
+// let b=a.length // 
+// document.writeln(b)
+
+
+
+// 2. charAt() : 
+// let a="groot academy"
+// let b=a.charAt(4) 
+// document.writeln(b)
+
+
+// 3. at() : 
+// let a="groot academy"
+// let b=a.at(2) 
+// document.writeln(b)
+
+
+// 4. slice()  :
+
+// let a="groot academy"
+// let b=a.slice(1,6)
+// document.writeln(b)
+
+
+// 5. substring : 
+// let a="groot academy"
+// let b=a.substring(1,6)
+// document.writeln(b)
+
+
+// 6. touppercase() : 
+// let a="groot academy"
+// let b=a.toUpperCase()
+// document.writeln(b)
+
+
+// 7. tolowercase() : 
+
+// let a="GROOT ACADEMY"
+// let b=a.toLowerCase()
+// document.writeln(b)
+
+
+// 8. concat() :
+
+// let a="groot "
+// let b="academy "
+// let c=b.concat(a) 
+// document.writeln(c)
+
+
+// 9. trim() : 
+
+// let a="     hello            "
+// let c=a.trim() // remove extra space
+// let b=c.length // length 
+// document.writeln(b)
+
+
+// 10. trimstart() : 
+
+
+// 11. trimend() : 
+
+
+// 12.indexof : 
+// let a="jatin"
+// let b=a.indexOf("i")
+// document.writeln(b)
+
+
+// 13. search : 
+// let a="jatin"
+// let b=a.search("i")
+// document.writeln(b)
+
+
+// 14. includes : 
+
+// let a="jatin"
+// let b=a.includes("u")
+// document.writeln(b)
+
+
+// 15. startswith() : 
+
+// let a="jatin"
+// let b=a.startsWith("u")
+// document.writeln(b)
+
+
+// 16. enswith() : 
+// let a="jatin"
+// let b=a.endsWith("u")
+// document.writeln(b)
