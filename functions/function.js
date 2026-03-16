5aae9661dbdbc28e11dec49ac3885be03986da6c
jatink2000 <jatink8059@gmail.com>
2026-03-16 12:00:51 +0530

    sh

--- functions/function.js
+++ functions/function.js
@@ -124,11 +124,115 @@
 
     // 4. with return with argument : 
 
-    function groot(myname){
-        return(myname)
-    }
+    // function groot(myname){
+    //     return(myname)
+    // }
     
-    document.writeln(groot("java script"))
+    // document.writeln(groot("java script"))
 
 
-    
+
+
+    // return : 
+
+    // function groot() {
+    //     let myname="groot software"
+
+    //     return(myname)
+    // }
+
+    // // let a=
+    // document.writeln(groot())
+
+
+
+
+    // 1. anonymous function : in an anonymous function , the function name is not define , to create the function first declare the function keyword , then (). start the function body using the {}. 
+
+        // syntex : 
+            // function (){
+            //     code to be excute 
+            // }
+
+
+            // let a=function (){
+            //     document.writeln("groot")
+            // }
+
+            // a() 
+
+
+
+    // 2. arrow functions : in an arrow function , there is no declare to function keyword , to create the arrow function define the function name using the = . then use the () followed by the => . and then start the function body using the {}. 
+
+        // syntex : 
+        // functionname=()=>{
+        //     code to be excute .  
+        // }
+
+
+        // groot=()=>{
+        //     document.writeln("hello groot")
+        //     document.writeln("hello groot")
+        //     document.writeln("hello groot")
+        // }
+
+        // groot()
+
+
+
+        // groot1=()=> document.writeln("hello java script")
+
+        // groot1()
+
+
+
+    // 3. self invoked function : the self invoked function is used to work /run when they define the function .  to create the function write the anonymous function inside the () . and then to call the function using the () . 
+
+
+        // (function (){
+        //     code to be excute
+        // })() 
+
+
+        // (function (){
+        //     document.writeln("hello jatin")
+        // })() ; 
+
+
+
+
+
+
+    // Object : object is used to store the multiple key and value pair inside the {} . 
+
+        // let a={
+        //     key:value , 
+        //     key2:value2,
+        //     .
+        //     .
+        //     .
+        // }
+
+
+
+        // let student={
+        //     "name":"ravi kumar",
+        //     "city":"jaipur",
+        //     "course":"web designing"
+        // }
+
+        // document.writeln(student.name)
+        // document.writeln(student.city)
+        // document.writeln(student.course)
+
+
+
+        let a=new Object() ;
+
+        a.name="groot"
+        a.city="jaipur"
+
+
+        document.writeln(a.name)
+        document.writeln(a.city)
